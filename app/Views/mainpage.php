9c2d5c1ee3b3ffca9ca0466a3e74c368504e697e
AlfianWD <carzyman01@gmail.com>
2023-04-03 18:20:26 +0700

    Menambahkan Favicon Website

--- app/Views/mainpage.php
+++ app/Views/mainpage.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Pusat Bisnis UINSA</title>
 
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/fav-uinsa-logo.png" type="image/x-icon">
+
     <!-- Boostrap-->
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
@@ -858,6 +861,6 @@
       integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
       crossorigin="anonymous">
     </script>
-    
+
   </body>
 </html>
